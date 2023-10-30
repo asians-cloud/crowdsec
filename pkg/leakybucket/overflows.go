a6932dbc5a2f6877cd16025b6d25e241c557be3f
victor <Andy>
2023-10-29 23:13:11 -0700

    manually merge labels code from codebase, but sse is removed.

--- pkg/leakybucket/overflows.go
+++ pkg/leakybucket/overflows.go
@@ -304,7 +304,7 @@ func NewAlert(leaky *Leaky, queue *Queue) (types.RuntimeAlert, error) {
 		newApiAlert := apiAlert
 		srcCopy := srcValue
 		newApiAlert.Source = &srcCopy
-		if v, ok := leaky.BucketConfig.Labels["remediation"]; ok && v == "true" {
+		if v, ok := leaky.BucketConfig.Labels["remediation"]; ok && v == true {
 			newApiAlert.Remediation = true
 		}
 
