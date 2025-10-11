fec8cb850a037cfa609b66b9f5035ec768b93b57
Mahadi-101 <mahadi.taronno@asians.cloud>
2025-10-11 16:11:39 +0600

    Add /v1/bouncers/add for remote bouncer registration

--- pkg/apiserver/controllers/controller.go
+++ pkg/apiserver/controllers/controller.go
@@ -89,6 +89,7 @@ func (c *Controller) NewV1() error {
 	groupV1.POST("/watchers", c.HandlerV1.AbortRemoteIf(c.DisableRemoteLapiRegistration), c.HandlerV1.CreateMachine)
 	groupV1.POST("/watchers/login", c.HandlerV1.Middlewares.JWT.Middleware.LoginHandler)
 	groupV1.POST("/watchers/validate", c.HandlerV1.ValidateMachine)
+	groupV1.POST("/bouncers/add", c.HandlerV1.AddBouncer)
 
 	jwtAuth := groupV1.Group("")
 	jwtAuth.GET("/refresh_token", c.HandlerV1.Middlewares.JWT.Middleware.RefreshHandler)
